9a6fb62772602c7e92fc71cee4023c3610a5d2cb
jrjeep <jrjeep@gmail.com>
2025-05-09 10:59:20 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,16 +4,17 @@ var outputArray = [];
 var perguntas;
 var x = 0;
 (function() {
-    const config = {
-	${{ secrets.SURVEYPERCYTESTSECRET_FB }}
-    /*apiKey: "AIzaSyDrQGvVqEEoM8sAtIC6DmKcu9N4dtic28g",
+    const config = ${{ secrets.SURVEYPERCYTESTSECRET_FB }};
+
+    /* {
+    apiKey: "AIzaSyDrQGvVqEEoM8sAtIC6DmKcu9N4dtic28g",
     authDomain: "surveypercy-3914b.firebaseapp.com",
     databaseURL: "https://surveypercy-3914b.firebaseio.com",
     projectId: "surveypercy-3914b",
     storageBucket: "surveypercy-3914b.appspot.com",
     messagingSenderId: "748138801996"
-    */
     };
+    */
     firebase.initializeApp(config);
 
     const dbEvaluationQuestRef = firebase.database().ref('Questionario');//.endAt('05');
