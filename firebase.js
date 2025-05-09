5c2b443ca06abf5aca2f4349c48d486cc1efd32e
jrjeep <jrjeep@gmail.com>
2025-05-09 11:07:33 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,8 +4,7 @@ var outputArray = [];
 var perguntas;
 var x = 0;
 (function() {	
-	
-    const config = {$({ secrets.SURVEYPERCYTESTSECRET_FB })};
+    const config = {$({ SURVEYPERCYTESTSECRET_FB })};
 
     /* {
     apiKey: "AIzaSyDrQGvVqEEoM8sAtIC6DmKcu9N4dtic28g",
