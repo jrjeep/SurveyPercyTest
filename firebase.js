746d5ef8c08b09b2798be01557496ee91567c064
jrjeep <jrjeep@gmail.com>
2025-05-09 11:21:52 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ var x = 0;
     const config = //$(secrets.SURVEYPERCYTESTSECRET_FB);
 
      {
-    apiKey: $(secrets.SURVEYPERCYTESTSECRET_FB),//"AIzaSyDrQGvVqEEoM8sAtIC6DmKcu9N4dtic28g",
+    apiKey: $(SURVEYPERCYTESTSECRET_FB),//"AIzaSyDrQGvVqEEoM8sAtIC6DmKcu9N4dtic28g",
     authDomain: "surveypercy-3914b.firebaseapp.com",
     databaseURL: "https://surveypercy-3914b.firebaseio.com",
     projectId: "surveypercy-3914b",
